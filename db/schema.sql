49631079c234714ef5eb104449dbde08c45aba99
Brian Campbell <brian.campbell003@gmail.com>
2022-10-07 16:21:30 -0500

    Classes, inquirer menu tree, sql connect

---
+++ db/schema.sql
@@ -0,0 +1,31 @@
+DROP DATABASE IF EXISTS organization_db;
+CREATE DATABASE organization_db;
+
+USE organization_db;
+
+CREATE TABLE depts (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE roles (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL(10,2) NOT NULL,
+  dept_id INT NOT NULL,
+  FOREIGN KEY (dept_id)
+  REFERENCES depts(id)
+  ON DELETE SET NULL
+);
+
+CREATE TABLE employees (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT
+  FOREIGN KEY (role_id)
+  REFERENCES roles(id)
+  ON DELETE SET NULL
+);
--- server.js
+++ server.js
@@ -1,3 +1,153 @@
+// Require packages
 const inquirer = require('inquirer');
 const consTable = require('console.table');
 const mysql = require('mysql2');
+// Require classes
+const Dept = require('./lib/Dept.js');
+const Role = require('./lib/Role.js');
+const Employee = require('./lib/Employee.js');
+
+// Establish a connection to the SQL databse
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      user: 'root',
+      password: 'sql_P4SS_0363',
+      database: 'organization_db'
+    },
+    console.log(`Connected to the organization_db database.`)
+  );
+
+//   I DONT THINK ILL USE THEsE EMPTY ARRAYS
+let myDepts = [];
+let myRoles = [];
+let myEmployees = [];
+
+mainMenu();
+
+// Main Menu to choose to an action
+function mainMenu() {
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                message: 'Select an action',
+                name: 'mainMenu',
+                choices: ['View all departments', 'View all roles', 'View all employees', 'Add department', 'Add role', 'Add employee']
+            }
+        ])
+        .then(response => {
+            switch (response.mainMenu) {
+                case 'View all departments':
+                    viewDepts();
+                    break;
+
+                case 'View all roles':
+                    viewRoles();
+                    break;
+
+                case 'View all employees':
+                    viewEmployees();
+                    break;
+
+                case 'Add department':
+                    addDept();
+                    break;
+
+                case 'Add role':
+                    addRole();
+                    break;
+
+                case 'Add employee':
+                    addEmployee();
+                    break;
+
+                default:
+                    break;
+            }
+        })
+}
+
+// Seperate JS files for these???:
+// function viewDepts() {}
+// function viewRoles() {}
+// function viewEmployees() {}
+
+// Add department, SOMETHING, then return to Main Menu
+function addDept() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: "Enter the name of the department",
+                name: 'deptName',
+            }
+        ])
+        .then(response => {
+            let { deptName } = response;
+            let dept = new Dept(deptName);
+            myDepts.push(dept);
+            mainMenu();
+        })
+}
+
+// Add role, SOMETHING, then return to Main Menu
+function addRole() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: "Enter the name of the new role",
+                name: 'roleName',
+            },
+            {
+                type: 'input',
+                message: "Enter the salary for the new role",
+                name: 'roleSalary',
+            },
+            {
+                type: 'input',
+                message: "Enter the department to which the new role belongs",
+                name: 'roleDept',
+            }
+        ])
+        .then(response => {
+            let { roleName, roleSalary, roleDept } = response;
+            let role = new Role(roleName, roleSalary, roleDept);
+            myRoles.push(role);
+            mainMenu();
+        })
+}
+
+// Add employee, SOMETHING, then return to Main Menu
+function addEmployee() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: "Enter the first name of the new employee",
+                name: 'empFirstName',
+            },
+            {
+                type: 'input',
+                message: "Enter the first name of the new employee",
+                name: 'empLastName',
+            },
+            {
+                type: 'input',
+                message: "Enter the new employee's role",
+                name: 'empRole',
+            },
+            {
+                type: 'input',
+                message: "Enter the new employee's manager",
+                name: 'empManager',
+            }
+        ])
+        .then(response => {
+            let { empFirstName, empLastName, empRole, empManager } = response;
+            let employee = new Employee(empFirstName, empLastName, empRole, empManager);
+            myEmployees.push(employee);
+            mainMenu();
+        })
+}
